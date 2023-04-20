6021de1f9d7b30dc71b9e7abe5c886e7f8c5795a
Itzel97 <itzel.alonso97@hotmail.com>
2023-04-20 12:16:04 -0600

    primer commit

---
+++ src/Variables.java
@@ -0,0 +1,39 @@
+public class Variables {
+
+    public static void main (String args[]){
+
+        //Variable defined
+        int myIntVariable = 10;
+        System.out.println(myIntVariable);
+
+        // Modified the value of the variable
+        myIntVariable = 22 ;
+        System.out.println(myIntVariable);
+
+        String myVariableString = "Hi";
+        System.out.println(myVariableString);
+
+        myVariableString = "Goodbye";
+        System.out.println(myVariableString);
+
+        //var - inference type in Java.
+        var myVariableInt2 = 15;
+        System.out.println(myVariableInt2);
+
+        var myVariableString2 = "Hello";
+        System.out.println("myVariableString2 = " + myVariableString2);
+
+        var _MiVariable = 3;
+        var miVariable = 1;
+        var $miVariable = 0;
+        // var áVariable = 2 ;  no recomendable
+
+        // var #miVariable = 2; illegal character not allow  to user
+
+
+    }
+
+
+
+
+}
